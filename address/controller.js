0936940e9bdac60ed619b4d35daba005f55da968
tarieli1 <tarieli1@gmail.com>
2017-03-01 16:42:30 +0200

    Update controller.js

--- address/controller.js
+++ address/controller.js
@@ -1,23 +1,21 @@
 const addressModel = require('./model');
-const {logger} = require('../lib');
-const cache = require('../lib/cache');
-
+const { logger, cache } = require('../lib');
 
 function get(id) {
-	cache.get(id, function( err, value ){
-		  if( err ){
-    		return addressModel.find({_id: id})
-			.then((address) => {
-			logger.info('Successfully got address from db');
-			cache.set(id, address, function( err, success ){
-  				if( !err && success ){
-    				logger.info('Successfully cached')
- 				 }
-			});
-			return address;
-		})
-		.catch(err => err);
-    	}
+	cache.get(id, function(err, value){
+		  if (err) {
+			return addressModel.find({_id: id})
+				.then((address) => {
+					logger.info('Successfully got address from db');
+					cache.set(id, address, function(err, success) {
+						if (!err && success) {
+							logger.info('Successfully insert to cache');
+						}
+					});
+					return address;
+			})
+			.catch(err => err);
+		}
 		else {
 			logger.info('retreived from cache')
 			return value;
