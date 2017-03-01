118e940329162416f3831b13682e1c3fde311592
tarieli1 <tarieli1@gmail.com>
2017-03-01 17:43:33 +0200

    Update controller.js

--- address/controller.js
+++ address/controller.js
@@ -1,13 +1,30 @@
+const promise = require("bluebird");
+
 const addressModel = require('./model');
 const { logger, cache } = require('../lib');
 
+const cacheGet = promise.promisify(cache.get, {context: cache});
+const cacheSet = promise.promisify(cache.set, {context: cache});
+
 function get(id) {
+	cacheGet(id)
+		.then((value) => {
+			logger.info('got data from cache');
+			return value;
+		})
+		.catch((err) => {
 			return addressModel.find({_id: id})
 				.then((address) => {
 					logger.info('Successfully got address from db');
-					return address;
+					cacheSet(id, address)
+						.then((res) => {
+							logger.info('Successfully insert to cache');
+							return address;
+						})
+						.catch(err => err);
 			})
 			.catch(err => err);
+			});
 }
 
 function create(addressParams) {
